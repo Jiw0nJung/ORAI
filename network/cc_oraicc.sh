c08c456fdadfe451c3898cd91fe8269a20cda20d
Jiw0nJung <spnoch@naver.com>
2020-06-11 17:29:17 +0900

    network update

--- network/cc_oraicc.sh
+++ network/cc_oraicc.sh
@@ -1,20 +1,21 @@
-
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/orai.com/orderers/orderer.orai.com/msp/tlscacerts/tlsca.orai.com-cert.pem
 
 # install
 docker exec cli peer chaincode install -n oraicc -v 1.0 -p github.com/oraicc
-
-# instantiate
-docker exec cli peer chaincode instantiate -n oraicc -v 1.0 -C orai -c '{"Args":[""]}' -P 'OR ("Org1MSP.member","Org2MSP.member","Org3MSP.member")'
 sleep 5
 
+# instantiate
+docker exec cli peer chaincode instantiate -o orderer.orai.com:7050 --tls --cafile $ORDERER_CA -C orai -n oraicc -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member','Org3MSP.member')"
+sleep 10
+
 # addUser
-docker exec cli peer chaincode invoke -n oraicc -C orai -c '{"Args":["addUser","jiwon","audi a8","audi"]}'
+docker exec cli peer chaincode invoke -o orderer.orai.com:7050 --tls --cafile $ORDERER_CA -C orai -n oraicc -C orai -c '{"Args":["addUser","jiwon","audi a8","audi"]}'
 sleep 5
 
 # invoke
-docker exec cli peer chaincode invoke -n oraicc -C orai -c '{"Args":["addData","jiwon","raining","daylight","wet","flooded","backing","rearend"]}'
+docker exec cli peer chaincode invoke -o orderer.orai.com:7050 --tls --cafile $ORDERER_CA -C orai -n oraicc -C orai -c '{"Args":["addAccidents","jiwon","raining","daylight","wet","flooded","backing","rearend"]}'
 sleep 5
 
 # query user0
-docker exec cli peer chaincode query -n oraicc -C orai -c '{"Args":["readData","jiwon"]}'
+docker exec cli peer chaincode query -n oraicc -C orai -c '{"Args":["viewAccidents","jiwon"]}'
 sleep 5
--- network/start.sh
+++ network/start.sh
@@ -10,6 +10,12 @@ set -ev
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/orai.com/orderers/orderer.orai.com/msp/tlscacerts/tlsca.orai.com-cert.pem
+# PEER0_ORG1_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.user.com/peers/peer0.org1.user.com/tls/ca.crt
+# PEER0_ORG2_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.insurance.com/peers/peer0.org2.insurance.com/tls/ca.crt
+# PEER0_ORG3_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.manufacturer.com/peers/peer0.org3.manufacturer.com/tls/ca.crt
+
+
 docker-compose -f docker-compose.yml down
 
 docker-compose -f docker-compose.yml up -d
@@ -21,7 +27,7 @@ export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec cli peer channel create -o orderer.orai.com:7050 -c orai -f /etc/hyperledger/configtx/channel.tx
+docker exec cli peer channel create -o orderer.orai.com:7050 --tls --cafile $ORDERER_CA -c orai -f /etc/hyperledger/configtx/channel.tx
 
 sleep 5
 # Join peer0.org1.user.com to the channel.
@@ -32,11 +38,6 @@ sleep 5
 docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.insurance.com/msp" peer0.org2.insurance.com peer channel join -b /etc/hyperledger/configtx/orai.block
 sleep 5
 
-
 # Join peer0.org3.manufacturer.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org3.manufacturer.com/msp" peer0.org3.manufacturer.com peer channel join -b /etc/hyperledger/configtx/orai.block
 sleep 5
-
-
-
-
